f34b8aa9b67cf84f4042abffd24e1bfac64897c0
Wassey Development <wasseydev@gmail.com>
2016-09-19 21:04:31 +1200

    Add initial constructors to Jasper class

--- Converter/Jasper.cs
+++ Converter/Jasper.cs
@@ -4,9 +4,51 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Word = Microsoft.Office.Interop.Word;
+
 namespace Converter
 {
     public class Jasper
     {
+        /// <summary>
+        /// The Word Application containing the documents that are being converted.
+        /// </summary>
+        Word.Application WordApp;
+        /// <summary>
+        /// If only a single document is being converted, then this is it.
+        /// </summary>
+        Word.Document WordDoc;
+        
+        /// <summary>
+        /// Constructor that takes a Word.Application reference. In this case, the
+        /// document is considered to be the active document. If there is no active
+        /// document, an exception is automatically thrown.
+        /// </summary>
+        /// <param name="wordApp">Word.Application</param>
+        public Jasper(Word.Application wordApp)
+        {
+            WordApp = wordApp;
+            WordDoc = WordApp.ActiveDocument;
+        }
+
+        /// <summary>
+        /// Constructor that takes a Word.Application reference and a Document. If either
+        /// parameter is null, an exception is thrown.
+        /// </summary>
+        /// <param name="wordApp">Word.Application</param>
+        /// <param name="wordDoc">Word.Document</param>
+        public Jasper(Word.Application wordApp, Word.Document wordDoc)
+        {
+            WordApp = wordApp;
+            WordDoc = wordDoc;
+            if (WordApp == null)
+            {
+                throw new Exception("Word Application cannot be null");
+            }
+            else if (WordDoc == null)
+            {
+                throw new Exception("No active document");
+            }
+        }
     }
 }
